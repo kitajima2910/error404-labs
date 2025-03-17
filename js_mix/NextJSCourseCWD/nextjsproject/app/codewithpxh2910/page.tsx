6e629c437aefe8d1666b0996acf403a4ebba1548
kitajima2910 <kitajima2910@gmail.com>
2025-03-17 21:08:50 +0700

    - routing

---
+++ js_mix/NextJSCourseCWD/nextjsproject/app/codewithpxh2910/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CodeWithPXH2910 = () => {
+    return <div>CodeWithPXH2910</div>;
+};
+
+export default CodeWithPXH2910;
---
+++ js_mix/NextJSCourseCWD/nextjsproject/app/pxh2910Channel/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PXH2910Channel = () => {
+  return (
+    <div>PXH2910Channel</div>
+  )
+}
+
+export default PXH2910Channel
