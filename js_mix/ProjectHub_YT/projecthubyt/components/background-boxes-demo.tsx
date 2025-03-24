321797dcfd40af9e76acb5390ac8e9db55cf79b1
kitajima2910 <kitajima2910@gmail.com>
2025-03-24 22:19:55 +0700

    - fix css text on boxes

--- js_mix/ProjectHub_YT/projecthubyt/components/background-boxes-demo.tsx
+++ js_mix/ProjectHub_YT/projecthubyt/components/background-boxes-demo.tsx
@@ -8,8 +8,8 @@ export default function BackgroundBoxesDemo() {
             <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
             <Boxes />
-            <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>Tailwind is Awesome</h1>
-            <p className="text-center mt-2 text-neutral-300 relative z-20">Framer motion is the best animation library ngl</p>
+            <h1 className="text-white text-7xl uppercase z-20 bg-black pt-5 pb-5 pl-10 pr-10 rounded-4xl font-bold mb-4">Tailwind is Awesome</h1>
+            <p className="text-white font-bold text-2xl z-20">Framer motion is the best animation library ngl</p>
         </div>
     );
 }
