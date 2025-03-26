e148dbbf796afda271f0d8910e92d90f38c9a574
kitajima2910 <kitajima2910@gmail.com>
2025-03-26 16:24:55 +0700

    - project section

--- js_mix/ProjectHub_YT/projecthubyt/app/(root)/page.tsx
+++ js_mix/ProjectHub_YT/projecthubyt/app/(root)/page.tsx
@@ -1,9 +1,26 @@
 import BackgroundBoxesDemo from "@/components/background-boxes-demo";
+import { ThreeDCard } from "@/components/ThreeDCard";
 
 const page = () => {
     return (
         <>
             <BackgroundBoxesDemo />
+
+            <section className="container mx-auto w-[100cqw] bg-pink-50">
+                <p className="font-semibold text-[30px]">Trendy Projects</p>
+                <ul className="mt-5 card_grid grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-1 justify-center">
+                    {[
+                        { id: 123, title: "a new project", desc: "a greate descrip" },
+                        { id: 123, title: "a new project", desc: "a greate descrip" },
+                        { id: 123, title: "a new project", desc: "a greate descrip" },
+                        { id: 123, title: "a new project", desc: "a greate descrip" },
+                    ].map((item, i) => (
+                        <li key={i}>
+                            <ThreeDCard />
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </>
     );
 };
--- js_mix/ProjectHub_YT/projecthubyt/components/background-boxes-demo.tsx
+++ js_mix/ProjectHub_YT/projecthubyt/components/background-boxes-demo.tsx
@@ -4,7 +4,7 @@ import SearchForm from "./SearchForm";
 
 export default function BackgroundBoxesDemo() {
     return (
-        <div className="h-[730px] relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
+        <div className="h-[530px] relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
             <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
             <Boxes />
