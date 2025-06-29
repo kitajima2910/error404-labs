5927bd60c1f665bef594d3d8f73c740012500233
kitajima2910 <kitajima2910@gmail.com>
2025-06-29 22:51:59 +0700

    - UnityProject, MarkerAR, ARBasics

---
+++ game_mix/ARBasics/Assets/Scripts/CubeRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+    Vector3 rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = new Vector3(0, 600, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotation * Time.deltaTime);
+    }
+}
