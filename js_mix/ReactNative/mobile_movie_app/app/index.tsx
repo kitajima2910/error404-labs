93925ff1e01db3dbef3173f61d2a4348b570a065
kitajima2910 <kitajima2910@gmail.com>
2025-07-01 23:23:01 +0700

    - React Native Course for Beginners in 2025 | Build a Full Stack React Native App (00:35:55)

---
+++ js_mix/ReactNative/mobile_movie_app/app/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components/native";
+
+export default function Index() {
+    return (
+        <View>
+            <Text>Welcome!</Text>
+        </View>
+    );
+}
+
+const View = styled.View`
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Text = styled.Text`
+	color: #ab8bff;
+	font-size: 38px;
+	font-weight: 500;
+`;
