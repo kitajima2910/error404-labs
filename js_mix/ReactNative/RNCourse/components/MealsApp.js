30c0c1a9c33ed9fe85a7385d7b15e6cdda7d7d21
kitajima2910 <kitajima2910@gmail.com>
2025-07-10 16:10:43 +0700

    - 102. Styling Screen Headers & Backgrounds

--- js_mix/ReactNative/RNCourse/components/MealsApp.js
+++ js_mix/ReactNative/RNCourse/components/MealsApp.js
@@ -1,27 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import CategoriesScreen from '../screens/CategoriesScreen'
-import { StatusBar } from 'expo-status-bar'
-import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import MealsOverviewScreen from '../screens/MealsOverviewScreen'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import CategoriesScreen from "../screens/CategoriesScreen";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverviewScreen from "../screens/MealsOverviewScreen";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 const MealsApp = () => {
     return (
         <>
             <StatusBar style="dark" />
             <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
-                    <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
+                <Stack.Navigator
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: "#351401",
+                        },
+                        headerTintColor: "white",
+                        contentStyle: {
+                            backgroundColor: "#3f2f25",
+                        },
+                    }}
+                >
+                    <Stack.Screen
+                        name="MealsCategories"
+                        component={CategoriesScreen}
+                        options={{
+                            title: "All Categories",
+                        }}
+                    />
+                    <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
                 </Stack.Navigator>
             </NavigationContainer>
         </>
-    )
-}
+    );
+};
 
-export default MealsApp
+export default MealsApp;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- js_mix/ReactNative/RNCourse/screens/MealsOverviewScreen.js
+++ js_mix/ReactNative/RNCourse/screens/MealsOverviewScreen.js
@@ -1,35 +1,38 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { MEALS } from '../data/dummy-data'
-import MealItem from '../components/MealItem'
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 const MealsOverviewScreen = ({ route }) => {
+    const catId = route.params.categoryId;
 
-    const catId = route.params.categoryId
+    const displayMeals = MEALS.filter((meal) => meal.categoryIds.indexOf(catId) >= 0);
 
-    const displayMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0)
+    const renderMealItem = ({ item }) => {
+        const mealItemPrps = {
+            title: item.title,
+            imageUrl: item.imageUrl,
+            duration: item.duration,
+            affordability: item.affordability,
+            complexity: item.complexity,
+        };
 
-    const renderMealItem = (itemData) => {
-        return <MealItem title={itemData.item.title} />
-    }
+        return <MealItem {...mealItemPrps} />;
+    };
 
     return (
         <View style={styles.container}>
-            <FlatList 
-                data={displayMeals}
-                renderItem={renderMealItem}
-                keyExtractor={(item) => item.id}
-            />
+            <FlatList data={displayMeals} renderItem={renderMealItem} keyExtractor={(item) => item.id} />
         </View>
-    )
-}
+    );
+};
 
-export default MealsOverviewScreen
+export default MealsOverviewScreen;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
-    }
-})
+        alignItems: "center",
+    },
+});
