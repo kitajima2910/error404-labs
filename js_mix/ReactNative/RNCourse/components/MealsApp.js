3d6e1ea94f92b945f01c0c110484d79c423dbe8a
kitajima2910 <kitajima2910@gmail.com>
2025-07-11 12:37:29 +0700

    - 117. Managing App-wide State with Context

--- js_mix/ReactNative/RNCourse/components/MealsApp.js
+++ js_mix/ReactNative/RNCourse/components/MealsApp.js
@@ -9,6 +9,7 @@ import MealDetailScreen from "../screens/MealDetailScreen";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import FavoritesScreen from "../screens/FavoritesScreen";
 import { Ionicons } from "@expo/vector-icons";
+import FavoritesContextProvider from "../store/context/favorites-context";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -61,30 +62,33 @@ const MealsApp = () => {
     return (
         <>
             <StatusBar style="light" />
-            <NavigationContainer>
-                <Stack.Navigator
-                    screenOptions={{
-                        headerStyle: {
-                            backgroundColor: "#351401",
-                        },
-                        headerTintColor: "white",
-                        contentStyle: {
-                            backgroundColor: "#3f2f25",
-                        },
-                    }}
-                >
-                    <Stack.Screen
-                        name="Drawer"
-                        component={DrawerNavigator}
-                        options={{
-                            title: "All Categories",
-                            headerShown: false,
+
+            <FavoritesContextProvider>
+                <NavigationContainer>
+                    <Stack.Navigator
+                        screenOptions={{
+                            headerStyle: {
+                                backgroundColor: "#351401",
+                            },
+                            headerTintColor: "white",
+                            contentStyle: {
+                                backgroundColor: "#3f2f25",
+                            },
                         }}
-                    />
-                    <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-                    <Stack.Screen name="MealDetail" component={MealDetailScreen} options={{ title: "About the Meal" }} />
-                </Stack.Navigator>
-            </NavigationContainer>
+                    >
+                        <Stack.Screen
+                            name="Drawer"
+                            component={DrawerNavigator}
+                            options={{
+                                title: "All Categories",
+                                headerShown: false,
+                            }}
+                        />
+                        <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+                        <Stack.Screen name="MealDetail" component={MealDetailScreen} options={{ title: "About the Meal" }} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </FavoritesContextProvider>
         </>
     );
 };
---
+++ js_mix/ReactNative/RNCourse/store/context/favorites-context.js
@@ -0,0 +1,29 @@
+import { createContext, useState } from "react";
+
+export const FavoritesContext = createContext({
+    ids: [],
+    addFavorite: (id) => {},
+    removeFavorite: (id) => {},
+});
+
+const FavoritesContextProvider = ({ children }) => {
+    const [favoriteMealIds, setFavoriteMealIds] = useState();
+
+    const addFavoriteHandler = (id) => {
+        setFavoriteMealIds((currentIds) => [...currentIds, id]);
+    };
+
+    const removeFavoriteHandler = (id) => {
+        setFavoriteMealIds((currentIds) => currentIds.filter((mealId) => mealId !== id));
+    };
+
+    const value = {
+        ids: favoriteMealIds,
+        addFavorite: addFavoriteHandler,
+        removeFavorite: removeFavoriteHandler,
+    };
+
+    return <FavoritesContext.Provider value={value}>{children}</FavoritesContext.Provider>;
+};
+
+export default FavoritesContextProvider;
