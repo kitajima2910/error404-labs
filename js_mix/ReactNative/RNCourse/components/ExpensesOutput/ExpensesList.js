6fa726722de26ec29ace5227a3613c84233d2f8f
kitajima2910 <kitajima2910@gmail.com>
2025-07-14 17:13:29 +0700

    - Working on Expense List Items

--- js_mix/ReactNative/RNCourse/components/ExpensesOutput/ExpensesList.js
+++ js_mix/ReactNative/RNCourse/components/ExpensesOutput/ExpensesList.js
@@ -1,8 +1,10 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import ExpensesItem from "./ExpensesItem";
 
 const renderExpenseItem = (itemData) => {
-    return <Text>{itemData.item.description}</Text>;
+    // return <Text>{itemData.item.description}</Text>;
+    return <ExpensesItem description={itemData.item.description} amount={itemData.item.amount} date={itemData.item.date} />;
 };
 
 const ExpensesList = ({ expenses }) => {
--- js_mix/ReactNative/RNCourse/components/ExpensesOutput/ExpensesOutput.js
+++ js_mix/ReactNative/RNCourse/components/ExpensesOutput/ExpensesOutput.js
@@ -35,6 +35,48 @@ const DUMMY_EXPENSES = [
         amount: 59.99,
         date: new Date("2021-12-19"),
     },
+    {
+        id: "e6",
+        description: "A pair of trousers",
+        amount: 89.99,
+        date: new Date("2021-12-19"),
+    },
+    {
+        id: "e7",
+        description: "Some bananas",
+        amount: 5.99,
+        date: new Date("2021-12-19"),
+    },
+    {
+        id: "e8",
+        description: "A book",
+        amount: 14.99,
+        date: new Date("2021-12-19"),
+    },
+    {
+        id: "e9",
+        description: "A pair of shoes",
+        amount: 59.99,
+        date: new Date("2021-12-19"),
+    },
+    {
+        id: "e10",
+        description: "A pair of trousers",
+        amount: 89.99,
+        date: new Date("2021-12-19"),
+    },
+    {
+        id: "e11",
+        description: "Some bananas",
+        amount: 5.99,
+        date: new Date("2021-12-19"),
+    },
+    {
+        id: "e12",
+        description: "A book",
+        amount: 14.99,
+        date: new Date("2021-12-19"),
+    },
 ];
 
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
