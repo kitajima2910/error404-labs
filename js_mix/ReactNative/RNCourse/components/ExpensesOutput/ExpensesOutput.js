625d1cc9f670f9c8a91deaf9867332ae27e36ca6
kitajima2910 <kitajima2910@gmail.com>
2025-07-14 16:42:16 +0700

    - 131. Adding Dummy Expense Data

--- js_mix/ReactNative/RNCourse/components/ExpensesOutput/ExpensesOutput.js
+++ js_mix/ReactNative/RNCourse/components/ExpensesOutput/ExpensesOutput.js
@@ -3,10 +3,43 @@ import React from "react";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
+const DUMMY_EXPENSES = [
+    {
+        id: "e1",
+        description: "A pair of shoes",
+        amount: 59.99,
+        date: new Date("2021-12-19"),
+    },
+    {
+        id: "e2",
+        description: "A pair of trousers",
+        amount: 89.99,
+        date: new Date("2021-12-19"),
+    },
+    {
+        id: "e3",
+        description: "Some bananas",
+        amount: 5.99,
+        date: new Date("2021-12-19"),
+    },
+    {
+        id: "e4",
+        description: "A book",
+        amount: 14.99,
+        date: new Date("2021-12-19"),
+    },
+    {
+        id: "e5",
+        description: "A pair of shoes",
+        amount: 59.99,
+        date: new Date("2021-12-19"),
+    },
+];
+
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
     return (
         <View>
-            <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+            <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
             <ExpensesList />
         </View>
     );
