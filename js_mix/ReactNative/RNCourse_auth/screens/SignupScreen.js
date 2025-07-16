897d42fb12f416457c1a114f94bdce87936cffff
kitajima2910 <kitajima2910@gmail.com>
2025-07-16 20:39:51 +0700

    - 182. Extracting the Authentication Token

--- js_mix/ReactNative/RNCourse_auth/screens/SignupScreen.js
+++ js_mix/ReactNative/RNCourse_auth/screens/SignupScreen.js
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import { createUser } from "../utils/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
     const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+    const authCtx = useContext(AuthContext);
+
     const signupHandler = async ({ email, password }) => {
         setIsAuthenticating(true);
         try {
-            await createUser(email, password);
+            const token = await createUser(email, password);
+            authCtx.authenticate(token);
         } catch (error) {
             Alert.alert("Signup failed", "Could not create user. Please check your credentials or try again later.");
         }
