7d404cd21ff74495c5eba3feb412257308c72000
kitajima2910 <kitajima2910@gmail.com>
2025-07-16 21:33:39 +0700

    - 185. Accessing Protected Resources

--- js_mix/ReactNative/RNCourse_auth/screens/WelcomeScreen.js
+++ js_mix/ReactNative/RNCourse_auth/screens/WelcomeScreen.js
@@ -1,26 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
-  return (
-    <View style={styles.rootContainer}>
-      <Text style={styles.title}>Welcome!</Text>
-      <Text>You authenticated successfully!</Text>
-    </View>
-  );
+    const [fetchedMessages, setFetchedMessages] = useState();
+
+    const authCtx = useContext(AuthContext);
+    const token = authCtx.token;
+
+    useEffect(() => {
+        axios.get(`https://react-native-course-52e67-default-rtdb.asia-southeast1.firebasedatabase.app/message.json?auth=${token}`).then((response) => {
+            setFetchedMessages(response.data);
+        });
+    }, [token]);
+
+    return (
+        <View style={styles.rootContainer}>
+            <Text style={styles.title}>Welcome!</Text>
+            <Text>You authenticated successfully!</Text>
+            <Text>{fetchedMessages}</Text>
+        </View>
+    );
 }
 
 export default WelcomeScreen;
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 32,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
+    rootContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 32,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 8,
+    },
 });
