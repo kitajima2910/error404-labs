5ced89084032ccef7a99d17b30bbbbc4f482003e
kitajima2910 <kitajima2910@gmail.com>
2025-07-18 15:29:32 +0700

    - 3 - Adding Custom Icons to the Bottom Tab Bar in a TikTok Clone with React Native

--- js_mix/ReactNative/TikTokClone/App.js
+++ js_mix/ReactNative/TikTokClone/App.js
@@ -11,17 +11,63 @@ import InboxScreen from "./screens/tabs/InboxScreen";
 import ProfileScreen from "./screens/tabs/ProfileScreen";
 import LoginScreen from "./screens/LoginScreen";
 
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import { View } from "react-native";
+
 const Stack = createNativeStackNavigator();
 const BootomTabs = createBottomTabNavigator();
 
 const TabsBootomTabs = () => {
     return (
-        <BootomTabs.Navigator initialRouteName="Home">
-            <BootomTabs.Screen name="Home" component={HomeScreen} />
-            <BootomTabs.Screen name="Friends" component={FriendsScreen} />
-            <BootomTabs.Screen name="Camera" component={CameraScreen} />
-            <BootomTabs.Screen name="Inbox" component={InboxScreen} />
-            <BootomTabs.Screen name="Profile" component={ProfileScreen} />
+        <BootomTabs.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+                tabBarActiveTintColor: "black",
+                tabBarInactiveTintColor: "gray",
+                headerShown: false,
+            }}
+        >
+            <BootomTabs.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <Ionicons name={focused ? "home-sharp" : "home-outline"} size={24} color="black" />,
+                }}
+            />
+            <BootomTabs.Screen
+                name="Friends"
+                component={FriendsScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <Ionicons name={focused ? "people-sharp" : "people-outline"} size={24} color="black" />,
+                }}
+            />
+            <BootomTabs.Screen
+                name="Camera"
+                component={CameraScreen}
+                options={{
+                    title: "",
+                    tabBarIcon: () => (
+                        <View className="absolute bottom-[-1/2] w-20 h-20 rounded-full justify-center items-center">
+                            <Ionicons name="add-circle-sharp" size={70} color="black" />
+                        </View>
+                    ),
+                }}
+            />
+            <BootomTabs.Screen
+                name="Inbox"
+                component={InboxScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <Ionicons name={focused ? "chatbubbles-sharp" : "chatbubbles-outline"} size={24} color="black" />,
+                }}
+            />
+            <BootomTabs.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => <Ionicons name={focused ? "person-sharp" : "person-outline"} size={24} color="black" />,
+                }}
+            />
         </BootomTabs.Navigator>
     );
 };
