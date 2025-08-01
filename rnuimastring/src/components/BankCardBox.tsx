2933bc6634956365fc2eac94f5f4c777ae2afd33
kitajima2910 <kitajima2910@gmail.com>
2025-08-01 16:32:51 +0700

    - Animations

--- rnuimastring/src/components/BankCardBox.tsx
+++ rnuimastring/src/components/BankCardBox.tsx
@@ -1,12 +1,22 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { s, vs } from "react-native-size-matters";
+import { ms, s, vs } from "react-native-size-matters";
 import { CardIcon } from "../assets/icons";
+import LottieView from 'lottie-react-native';
 
 const BankCardBox = () => {
     return (
         <View style={styles.container}>
-            <CardIcon style={{ marginBottom: vs(18) }} />
+            {/* <CardIcon style={{ marginBottom: vs(18) }} /> */}
+            <LottieView 
+                autoPlay
+                style={{
+                    width: ms(168),
+                    height: ms(106),
+                    marginBottom: vs(10)
+                }}
+                source={require("../assets/BankCardAnimation.json")}
+            />
             <Text style={{ marginBottom: vs(18), fontFamily: "Montserrat-SemiBold", fontWeight: "700", fontSize: s(16) }}>No master card added</Text>
             <Text style={{ fontFamily: "Montserrat-Regular", fontWeight: "400", fontSize: s(15), textAlign: "center", letterSpacing: 0.5, width: s(286) }}>You can add a mastercard and save it for later</Text>
         </View>
