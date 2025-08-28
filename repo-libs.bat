ebee5da2da9eb4da942c0c898803902c8485aad5
kitajima2910 <kitajima2910@gmail.com>
2025-08-28 23:00:23 +0700

    ........

--- repo-libs.bat
+++ repo-libs.bat
@@ -1,21 +1,6 @@
+
 @echo off
 setlocal enabledelayedexpansion
-
-rem --- Kiểm tra Git ---
-where git >nul 2>nul
-if %errorlevel% neq 0 (
-    echo Git is not installed or not in PATH.
-    pause
-    exit /b 1
-)
-
-for /f "delims=" %%G in ('where git') do (
-    set "GIT=%%G"
-    goto :break
-)
-:break
-echo Using Git at: %GIT%
-echo.
 
 REM Đọc từng dòng trong repo-libs.txt
 for /f "tokens=1,2" %%a in (repo-libs.txt) do (
@@ -23,6 +8,5 @@ for /f "tokens=1,2" %%a in (repo-libs.txt) do (
     git submodule add %%a %%b
 )
 
-echo.
 echo Done adding submodules!
 pause
