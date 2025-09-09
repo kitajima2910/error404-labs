c18847208e3aeff508936dae941379849df48a2e
kitajima2910 <kitajima2910@gmail.com>
2025-09-09 22:09:18 +0700

    - Signals

--- g/jumpstart-to-2d-game-development-godot-4-for-beginners/GemCatcher/Scenes/Game/Game.gd
+++ g/jumpstart-to-2d-game-development-godot-4-for-beginners/GemCatcher/Scenes/Game/Game.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 
-
-
 func _on_paddle_area_entered(area: Area2D) -> void:
 	#print("area: ", area)
+	pass # Replace with function body.
+
+
+func _on_gem_off_screen() -> void:
+	print("Game Over")
 	pass # Replace with function body.
--- g/jumpstart-to-2d-game-development-godot-4-for-beginners/GemCatcher/Scenes/Gem/Gem.gd
+++ g/jumpstart-to-2d-game-development-godot-4-for-beginners/GemCatcher/Scenes/Gem/Gem.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal gem_off_screen
+
 const SPEED: float = 100.0
 
 # Called when the node enters the scene tree for the first time.
@@ -13,11 +15,14 @@ func _process(delta: float) -> void:
 	
 	if position.y > get_viewport_rect().end.y:
 		#print("Gem falls off")
+		gem_off_screen.emit()
 		die()
+
 
 func die() -> void:
 	set_process(false)
 	queue_free()
+
 
 func _on_area_entered(area: Area2D) -> void:
 	#print("area: ", area)
