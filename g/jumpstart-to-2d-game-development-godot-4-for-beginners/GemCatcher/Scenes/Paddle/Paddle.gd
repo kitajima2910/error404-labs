73aac90ee0c032f9b671bb98cad4355569da6839
kitajima2910 <kitajima2910@gmail.com>
2025-09-09 21:07:30 +0700

    - Paddled input

---
+++ g/jumpstart-to-2d-game-development-godot-4-for-beginners/GemCatcher/Scenes/Paddle/Paddle.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+const SPEED: float = 200.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	if Input.is_action_pressed("move_left") == true:
+		position.x -= SPEED * delta
+	if Input.is_action_pressed("move_right") == true:
+		position.x += SPEED * delta
+	pass
