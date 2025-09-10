0526a542a236b572ec2510b725a9a6263a6a5bb6
kitajima2910 <kitajima2910@gmail.com>
2025-09-10 16:07:09 +0700

    - Building Our API

--- rn/100HoursWebDevelopmentBootcamp/MERNProductStore/backend/server.js
+++ rn/100HoursWebDevelopmentBootcamp/MERNProductStore/backend/server.js
@@ -1,18 +1,17 @@
-import express from "express"
-import dotenv from "dotenv"
-import { connectDB } from "./config/db.js"
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
-dotenv.config()
+import productRoutes from "./routes/product.route.js";
 
-const app = express()
+dotenv.config();
 
-app.get("/products", (req, res) => {
-    res.send("Hello from the backend")
-})
+const app = express();
+app.use(express.json());
 
-console.log(process.env.MONGO_URI)
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
-    connectDB()
-    console.log("Server is running on port 5000")
-})
+    connectDB();
+    console.log("Server is running on port 5000");
+});
