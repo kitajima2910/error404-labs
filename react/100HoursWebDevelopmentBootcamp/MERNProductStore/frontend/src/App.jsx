d48955637da54cab1fc4f69428c3859d7fe8807f
kitajima2910 <kitajima2910@gmail.com>
2025-09-15 14:39:55 +0700

    - navbar and create page

--- react/100HoursWebDevelopmentBootcamp/MERNProductStore/frontend/src/App.jsx
+++ react/100HoursWebDevelopmentBootcamp/MERNProductStore/frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import { Button } from '@chakra-ui/react'
+import { Box, Button, useColorModeValue } from "@chakra-ui/react";
+import { Route, Router, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return (
-    <>
-        <Button>Hello</Button>
-    </>
-  )
-}
+    return (
+        <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/create" element={<CreatePage />} />
+            </Routes>
+        </Box>
+    );
+};
 
-export default App
+export default App;
