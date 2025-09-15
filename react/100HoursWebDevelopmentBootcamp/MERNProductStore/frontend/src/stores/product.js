a7a1e038cca7081f68082129e1390c45d794cbc5
kitajima2910 <kitajima2910@gmail.com>
2025-09-15 17:47:36 +0700

    - update frontend

---
+++ react/100HoursWebDevelopmentBootcamp/MERNProductStore/frontend/src/stores/product.js
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+export const useProductStore = create((set) => ({
+    products: [],
+    setProducts: (products) => set({ products }),
+    createProduct: async (newProduct) => {
+        if (!newProduct.name || !newProduct.price || !newProduct.image) {
+            return {
+                success: false,
+                message: "Please fill in all fields.",
+            }
+        }
+
+
+    },
+}));
