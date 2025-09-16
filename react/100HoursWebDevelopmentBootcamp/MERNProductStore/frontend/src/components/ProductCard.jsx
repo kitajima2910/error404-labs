035cf57821bae8c3133f3a7280a487223c13d0aa
hoai.phamxuan <hoai.phamxuan@gameloft.com>
2025-09-16 18:31:16 +0700

    - home page

---
+++ react/100HoursWebDevelopmentBootcamp/MERNProductStore/frontend/src/components/ProductCard.jsx
@@ -0,0 +1,96 @@
+import { Box, Button, Heading, HStack, IconButton, Image, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useColorModeValue, useDisclosure, useToast, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import { useProductStore } from "../stores/product";
+
+const ProductCard = ({ product }) => {
+    const textColor = useColorModeValue("gray.600", "gray.200");
+    const bg = useColorModeValue("white", "gray.600");
+
+    const { deleteProducts, updateProduct } = useProductStore();
+    const toast = useToast();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [updatedProduct, setUpdatedProduct] = useState(product)
+
+    const handleDeleteProduct = async (pid) => {
+        const { success, message } = await deleteProducts(pid);
+
+        toast({
+            title: success ? "Success" : "Error",
+            description: message,
+            status: success ? "success" : "error",
+            duration: 3000,
+            isClosable: true,
+        });
+    };
+
+    const handleUpdateProduct = async (pid, updatedProduct) => {
+        const { success, message } = await updateProduct(pid, updatedProduct)
+        onClose()
+        toast({
+            title: success ? "Success" : "Error",
+            description: message,
+            status: success ? "success" : "error",
+            duration: 3000,
+            isClosable: true,
+        });
+    }
+
+    return (
+        <Box
+            shadow={"lg"}
+            rounded={"lg"}
+            overflow={"hidden"}
+            transition={"all .3s"}
+            _hover={{
+                transform: "translateY(-5px)",
+                shadow: "xl",
+            }}
+            bg={bg}
+        >
+            <Image src={product.image} alt={product.name} h={48} w={"full"} objectFit={"cover"} />
+
+            <Box p={4}>
+                <Heading as="h3" size={"md"} mb={2}>
+                    {product.name}
+                </Heading>
+
+                <Text fontWeight={"bold"} fontSize={"xl"} color={textColor} mb={4}>
+                    ${product.price}
+                </Text>
+
+                <HStack spacing={2}>
+                    <IconButton icon={<EditIcon />} onClick={onOpen} colorScheme="blue" />
+                    <IconButton icon={<DeleteIcon />} onClick={() => handleDeleteProduct(product._id)} colorScheme="red" />
+                </HStack>
+            </Box>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Create your account</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <VStack spacing={4}>
+                            <Input placeholder="Product Name" name="name" value={updatedProduct.name} 
+                            onChange={(e) => setUpdatedProduct({...updatedProduct, name: e.target.value})} />
+                            <Input placeholder="Product Price" name="price" value={updatedProduct.price} 
+                            onChange={(e) => setUpdatedProduct({...updatedProduct, price: e.target.value})} />
+                            <Input placeholder="Image URL" name="image" value={updatedProduct.image} 
+                            onChange={(e) => setUpdatedProduct({...updatedProduct, image: e.target.value})} />
+                        </VStack>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={() => handleUpdateProduct(product._id, updatedProduct)}>
+                            Update
+                        </Button>
+                        <Button onClick={onClose} variant={"ghost"}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </Box>
+    );
+};
+
+export default ProductCard;
--- react/100HoursWebDevelopmentBootcamp/MERNProductStore/frontend/src/stores/product.js
+++ react/100HoursWebDevelopmentBootcamp/MERNProductStore/frontend/src/stores/product.js
@@ -27,7 +27,60 @@ export const useProductStore = create((set, get) => ({
 
         return {
             success: true,
-            message: "Product created successfully"
-        }
+            message: "Product created successfully",
+        };
     },
+    fetchProducts: async () => {
+        const res = await fetch("/api/products");
+        const data = await res.json();
+        set({
+            products: data.message,
+        });
+    },
+    deleteProducts: async (pid) => {
+        const res = await fetch(`/api/products/${pid}`, {
+            method: "DELETE",
+        });
+
+        const data = await res.json();
+        if (!data.success) {
+            return {
+                success: false,
+                message: data.message,
+            };
+        }
+
+        set({
+            products: get().products.filter((product) => product._id !== pid),
+        });
+        return {
+            success: true,
+            message: data.message,
+        };
+    },
+    updateProduct: async (pid, updatedProduct) => {
+        const res = await fetch(`/api/products/${pid}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedProduct)
+        })
+
+        const data = await res.json()
+        if (!data.success) {
+            return {
+                success: false,
+                message: data.message,
+            };
+        }
+        
+        set({
+            products: get().products.map(product => product._id === pid ? data.message : product)
+        })
+        return {
+            success: true,
+            message: data.message,
+        };
+    }
 }));
