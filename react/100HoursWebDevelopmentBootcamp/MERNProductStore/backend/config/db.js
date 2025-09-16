49abdd24a3ce7dd629d3a38137ae8e1f709c8b23
kitajima2910 <kitajima2910@gmail.com>
2025-09-16 20:58:59 +0700

    - update .env

--- react/100HoursWebDevelopmentBootcamp/MERNProductStore/backend/config/db.js
+++ react/100HoursWebDevelopmentBootcamp/MERNProductStore/backend/config/db.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        const MONGO_URI = `${process.env.MONGO_HOST}${process.env.MONGO_USER}${process.env.MONGO_PASSWORD}${process.env.MONGO_DOMAIN}${process.env.MONGO_DATABASE_NAME}${process.env.MONGO_PARAMS}`;
+        const conn = await mongoose.connect(MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log(error)
-        process.exit(1)
+        console.log(error);
+        process.exit(1);
     }
-}
+};
