3771f784d1efbed28b207351eb70d24c1d65cb4f
kitajima2910 <kitajima2910@gmail.com>
2025-09-24 22:31:08 +0700

    - fix validateForm

--- react/100HoursWebDevelopmentBootcamp/fsrealtimechat/backend/src/index.js
+++ react/100HoursWebDevelopmentBootcamp/fsrealtimechat/backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173", // Replace with your frontend URL
+        origin: "http://localhost:5174", // Replace with your frontend URL
         credentials: true,
     })
 );
--- react/100HoursWebDevelopmentBootcamp/fsrealtimechat/frontend/src/store/useAuthStore.js
+++ react/100HoursWebDevelopmentBootcamp/fsrealtimechat/frontend/src/store/useAuthStore.js
@@ -50,7 +50,7 @@ export const useAuthStore = create((set, get) => ({
 
     logout: async () => {
         try {
-            await axiosInstance.get("/auth/logout");
+            await axiosInstance.post("/auth/logout");
             set({
                 authUser: null,
             });
