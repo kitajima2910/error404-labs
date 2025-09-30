066164a1024988126b1f59d424786f380b6c563c
hoai.phamxuan <hoai.phamxuan@gameloft.com>
2025-09-30 15:06:55 +0700

    - Sidebar

--- react/100HoursWebDevelopmentBootcamp/fsrealtimechat/backend/src/index.js
+++ react/100HoursWebDevelopmentBootcamp/fsrealtimechat/backend/src/index.js
@@ -25,7 +25,7 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`), connectDB();
