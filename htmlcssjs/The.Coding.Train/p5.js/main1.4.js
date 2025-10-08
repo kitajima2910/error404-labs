1f2a103180cf3a7cc9436f0c8f20cbad6c89ece0
kitajima2910 <kitajima2910@gmail.com>
2025-10-08 22:28:23 +0700

    - RGB Color

---
+++ htmlcssjs/The.Coding.Train/p5.js/main1.4.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 300);
+}
+
+function draw() {
+    background(100);
+
+    rectMode(CENTER);
+    fill(0, 0, 255);
+    stroke(0, 255, 0);
+    strokeWeight(5);
+    rect(200, 150, 150, 150);
+
+    fill(255, 0, 0, 100);
+    noStroke();
+    ellipse(100, 150, 100, 100);
+}
