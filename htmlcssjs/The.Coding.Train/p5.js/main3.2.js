66b54209cac776e217adcf4dc7c2b3c71c7a240c
kitajima2910 <kitajima2910@gmail.com>
2025-10-21 10:36:26 +0700

    - The Bouncing Ball

---
+++ htmlcssjs/The.Coding.Train/p5.js/main3.2.js
@@ -0,0 +1,26 @@
+var x;
+var speed;
+
+function setup() {
+    createCanvas(400, 400);
+
+    x = 0;
+    speed = 2;
+}
+
+function draw() {
+    background(0);
+
+    noFill();
+    stroke(255);
+    strokeWeight(4);
+    ellipse(x + 54, height / 2, 100, 100);
+
+    if (x + 100 > width) {
+        speed = -2;
+    } else if (x < 0) {
+        speed = 2;
+    }
+
+    x = x + speed;
+}
