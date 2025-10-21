d666dcb90c4fe4455399cb0e94c6863abe9ac989
kitajima2910 <kitajima2910@gmail.com>
2025-10-21 10:49:14 +0700

    - Else and Else if, AND and OR

--- htmlcssjs/The.Coding.Train/p5.js/main3.2.js
+++ htmlcssjs/The.Coding.Train/p5.js/main3.2.js
@@ -16,10 +16,14 @@ function draw() {
     strokeWeight(4);
     ellipse(x + 54, height / 2, 100, 100);
 
-    if (x + 100 > width) {
-        speed = -2;
-    } else if (x < 0) {
-        speed = 2;
+    // if (x + 100 > width) {
+    //     speed = -2;
+    // } else if (x < 0) {
+    //     speed = 2;
+    // }
+
+    if (x + 100 > width || x < 0) {
+        speed = -speed;
     }
 
     x = x + speed;
