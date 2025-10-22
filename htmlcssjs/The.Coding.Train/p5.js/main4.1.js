25298c2fbd6fb9d5ae03274956258c0749a34276
kitajima2910 <kitajima2910@gmail.com>
2025-10-22 20:37:13 +0700

    - Nested Loops

--- htmlcssjs/The.Coding.Train/p5.js/main4.1.js
+++ htmlcssjs/The.Coding.Train/p5.js/main4.1.js
@@ -7,16 +7,25 @@ function draw() {
     strokeWeight(4);
     stroke(255);
 
-    var x = 0;
+    // var x = 0;
 
-    while (x <= width) {
-        fill(0, 200, 255);
-        ellipse(x, 100, 25, 25);
-        x = x + 50;
-    }
+    // while (x <= width) {
+    //     fill(random(255), random(255), random(255));
+    //     ellipse(x, 100, 25, 25);
+    //     x = x + 50;
+    // }
 
-    for (var x = 0; x <= width; x = x + 50) {
-        fill(255, 0, 200);
-        ellipse(x, 300, 25, 25);
+    // for (var x = 0; x <= width; x = x + 50) {
+    //     for (var y = 0; y <= width; y = y + 50) {
+    //         fill(random(255), random(255), random(255));
+    //         ellipse(x, y, 25, 25);
+    //     }
+    // }
+
+    for (var x = 0; x <= mouseX; x = x + 50) {
+        for (var y = 0; y <= mouseY; y = y + 50) {
+            fill(random(255), random(255), random(255));
+            ellipse(x, y, 25, 25);
+        }
     }
 }
