00e61ba0067d8956f317ffe56a2d9283357ac191
kitajima2910 <kitajima2910@gmail.com>
2025-10-24 14:10:58 +0700

    - Task 1: Counter

---
+++ Svelte/Counter/src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	let count = $state(0);
+</script>
+
+<div class="wrapper">
+	<input type="number" bind:value={count} disabled />
+	<button onclick={() => count++}>Count</button>
+</div>
+
+<style>
+	.wrapper {
+		width: 100vw;
+		height: 100vh;
+
+		background-color: #15171b;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	input {
+		line-height: 42px;
+
+		background-color: #1f2427;
+		color: #fff;
+
+		border: none;
+
+		padding: 0 10px;
+	}
+
+	button {
+		line-height: 40px;
+		width: 100px;
+		margin-left: 10px;
+
+		background-color: #464d54;
+		color: #fff;
+
+		cursor: pointer;
+
+		border: none;
+
+		font-weight: bold;
+	}
+</style>
