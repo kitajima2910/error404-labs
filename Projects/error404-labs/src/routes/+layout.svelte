9b29e43641f0dee4d9bcdb635590a60b808cb79b
kitajima2910 <kitajima2910@gmail.com>
2025-10-27 23:27:00 +0700

    - Change favicon error404-labs

--- Projects/error404-labs/src/routes/+layout.svelte
+++ Projects/error404-labs/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import icon_pxh from '$lib/assets/icon_pxh.png';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/x-icon" href={icon_pxh} />
 </svelte:head>
 
 {@render children?.()}
