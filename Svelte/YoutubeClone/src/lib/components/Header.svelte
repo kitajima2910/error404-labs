eea459651c705eaf18d6b37a2afb499aa20b05fa
kitajima2910 <kitajima2910@gmail.com>
2025-10-28 23:12:45 +0700

    - First commit + Header

---
+++ Svelte/YoutubeClone/src/lib/components/Header.svelte
@@ -0,0 +1,20 @@
+<header class="sticky top-0 left-0 z-10 flex h-16 items-center justify-between bg-white px-4 py-2">
+	<div class="flex items-center gap-4">
+		<img src="/icons/bugerIcon.svg" alt="" />
+		<img src="/icons/ytLogo.svg" alt="" />
+	</div>
+	<div>search bar</div>
+	<div class="flex items-center gap-3">
+		<img
+			src="/icons/createIcon.svg"
+			class="cursor-pointer rounded-full p-3 hover:bg-gray-200"
+			alt=""
+		/>
+		<img
+			src="/icons/notIcon.svg"
+			class="cursor-pointer rounded-full p-3 hover:bg-gray-200"
+			alt=""
+		/>
+		<img src="/midcodekid.jpg" class="h-8 w-8 cursor-pointer rounded-full bg-gray-200" alt="" />
+	</div>
+</header>
