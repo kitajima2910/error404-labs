0d69075569c4d7e4d8faad387ac864b4b60af2ad
kitajima2910 <kitajima2910@gmail.com>
2025-11-11 15:00:19 +0700

    - layout.svelte file

--- Svelte/Blog/src/routes/+layout.svelte
+++ Svelte/Blog/src/routes/+layout.svelte
@@ -9,4 +9,17 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="relative container mx-auto p-4">
+	<!-- Header -->
+	<div class="mb-6 text-center">
+		<a href="/">
+			<img src="/icon_pxh.png" alt="PXH Logo" class="inline-block max-h-14" />
+			<div class="text-xl font-thin opacity-60">Programming Tutorials</div>
+		</a>
+	</div>
+	{@render children()}
+	<!-- Footer -->
+	<div class="mt-8 text-center text-sm opacity-40">
+		Copyright &copy; {new Date().getFullYear()}
+	</div>
+</div>
