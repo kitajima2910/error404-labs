651ac4fcde5f0645644277679d9463a6ccb8bf57
hoai.phamxuan <hoai.phamxuan@gameloft.com>
2025-11-24 14:35:16 +0700

    - Update ABOUT

--- Projects/error404-labs/src/routes/ve-toi/+page.svelte
+++ Projects/error404-labs/src/routes/ve-toi/+page.svelte
@@ -1,17 +1,5 @@
 <script lang="ts">
 	import 'github-markdown-css/github-markdown-light.css';
-	import { loadMarkdownRaw } from '../../utils/markdown';
-	import { onMount } from 'svelte';
-
-	let content: string = $state('⏳ Đang tải dữ liệu');
-
-	let { data } = $props();
-
-	const { dataABOUTRaw } = data;
-
-	onMount(() => {
-		content = loadMarkdownRaw(dataABOUTRaw);
-	});
 </script>
 
 <svelte:head>
@@ -20,11 +8,116 @@
 		as="image"
 		href="https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=kitajima2910&theme=github_dark"
 	/>
-	<link rel="preload" href="/about/imgs/computer_gif.webp" as="image" type="image/webp" />
 
 	<link rel="canonical" href="https://error404-labs.info.vn/ve-toi" />
 </svelte:head>
 
 <div class="markdown-body">
-	{@html content}
+	<h1 style="text-align: center; border-bottom: 1px solid #d1d9e0b3; padding-bottom: 9.6px">
+		~ 🔬 RnD | Research & Developer 🔧 ~
+	</h1>
+	<img
+		style="width: 100%; height: 100%;"
+		src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&amp;size=22&amp;duration=3000&amp;pause=1000&amp;center=true&amp;vCenter=true&amp;width=700&amp;lines=%F0%9F%91%8B+Hey+I'm+Kitajima2910+%7C+R%26D+Dev;💡+Code+is+Poetry%2C+Logic+is+Art.;🔧+Tools+don't+build+great+devs%2C+habits+do.;🧩+Fixing+a+bug+is+unlocking+a+story.;⏳+Keep+coding+until+magic+feels+normal."
+		alt="Typing SVG"
+	/>
+	<div
+		class="computer_gif"
+		style="
+				width: 250px;
+				height: 250px;
+                margin: 30px auto;
+			"
+	>
+		<img alt="TTTT0803" src="/about/imgs/computer_gif.webp" style="width: 100%; height: 100%;" />
+	</div>
+	<div
+		style="
+				display: flex;
+				justify-content: center;
+				gap: 10px;
+				flex-wrap: wrap;
+				margin-bottom: 30px;
+			"
+	>
+		<div style="width: 40px; height: 40px">
+			<img
+				src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"
+				style="width: 100%; height: 100%; object-fit: cover"
+				alt="java logo"
+			/>
+		</div>
+		<div style="width: 40px; height: 40px">
+			<img
+				src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
+				style="width: 100%; height: 100%; object-fit: cover"
+				alt="csharp logo"
+			/>
+		</div>
+		<div style="width: 40px; height: 40px">
+			<img
+				src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+				style="width: 100%; height: 100%; object-fit: cover"
+				alt="html5 logo"
+			/>
+		</div>
+		<div style="width: 40px; height: 40px">
+			<img
+				src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+				style="width: 100%; height: 100%; object-fit: cover"
+				alt="css3 logo"
+			/>
+		</div>
+		<div style="width: 40px; height: 40px">
+			<img
+				src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+				style="width: 100%; height: 100%; object-fit: cover"
+				alt="javascript logo"
+			/>
+		</div>
+		<div style="width: 40px; height: 40px">
+			<img
+				src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original.svg"
+				style="width: 100%; height: 100%; object-fit: cover"
+				alt="android logo"
+			/>
+		</div>
+		<div style="width: 40px; height: 40px">
+			<img
+				src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg"
+				style="width: 100%; height: 100%; object-fit: cover"
+				alt="androidstudio logo"
+			/>
+		</div>
+		<div style="width: 40px; height: 40px">
+			<img
+				src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tortoisegit/tortoisegit-original.svg"
+				style="width: 100%; height: 100%; object-fit: cover"
+				alt="tortoisegit logo"
+			/>
+		</div>
+		<div style="width: 40px; height: 40px">
+			<img
+				src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+				style="width: 100%; height: 100%; object-fit: cover"
+				alt="git logo"
+			/>
+		</div>
+		<div style="width: 40px; height: 40px">
+			<img
+				src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+				style="width: 100%; height: 100%; object-fit: cover"
+				alt="github logo"
+			/>
+		</div>
+	</div>
 </div>
+
+<style>
+	@media screen and (max-width: 300px) {
+		.computer_gif {
+            width: auto !important;
+            height: auto !important;
+		}
+	}
+</style>
