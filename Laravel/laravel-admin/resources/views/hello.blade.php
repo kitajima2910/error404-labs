3f6a218ce0a2e6911fe5bd1ccb5916b593c06e5c
kitajima2910 <kitajima2910@gmail.com>
2025-12-10 19:34:24 +0700

    - Bài 2: Hiểu rõ cấu trúc thư mục trong Laravel 12

---
+++ Laravel/laravel-admin/resources/views/hello.blade.php
@@ -0,0 +1,1 @@
+<h1>Hello, World! - hello.blade.php</h1>
--- Laravel/laravel-admin/routes/web.php
+++ Laravel/laravel-admin/routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return response()->json("Hello, World!");
+});
+
+Route::get("/test", function () {
+    // return response()->json("Test!");
+    return view("hello");
 });
