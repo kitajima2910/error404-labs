3b1b5771700517cec345a58c5b1e1028e1ea95e4
kitajima2910 <kitajima2910@gmail.com>
2025-12-12 18:07:56 +0700

    - Update Text TitleScreen.js

--- game/TestPhaserGame/scenes/TitleScreen.js
+++ game/TestPhaserGame/scenes/TitleScreen.js
@@ -2,13 +2,18 @@ class TitleScreen extends Phaser.Scene {
   preload() {}
 
   create() {
-    const title = this.add.text(400, 150, "Old School Tennis", {
+    const title = this.add.text(400, 150, "Quan Vot Kieu Co Dien", {
       fontSize: 30,
       fontFamily: '"Press Start 2P"',
     });
     title.setOrigin(0.5, 0.5);
 
-    this.add.text(170, 250, "Press any key to start (Space)", {
+    this.add.text(170, 250, "Nhan Phim 'Space' De Bat Dau", {
+      fontSize: 15,
+      fontFamily: '"Press Start 2P"',
+    });
+
+    this.add.text(120, 350, "Dieu Khien - Phim Mui Ten Len/Xuong", {
       fontSize: 15,
       fontFamily: '"Press Start 2P"',
     });
