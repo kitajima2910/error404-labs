dabc55bfafe7e79aaba3f334a758b81fdcbe4e69
kitajima2910 <kitajima2910@gmail.com>
2026-01-01 13:25:57 +0700

    - Fix code PrintingPatternUsingLoops.c

--- c/Test/PrintingPatternUsingLoops.c
+++ c/Test/PrintingPatternUsingLoops.c
@@ -1,45 +1,49 @@
 #include <stdio.h>
 
-void process(int a[][20], int k, int offset);
+void process(int size, int a[][size], int offset, int value);
 
 int main() {
-    int n = 2;
+    int n;
+    scanf("%d", &n);
+
     int size = 2 * n - 1;
-    int a[20][20] = {0};
+    int a[size][size];
+
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            a[i][j] = 0;
 
     int offset = 0;
-
-    for (int k = n; k >= 1; k--) {
-        process(a, k, offset);
-        offset++;
+    for (int value = n; value >= 1; value--) {
+        process(size, a, offset++, value);
     }
 
-    // in ma trận
     for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
-            printf("%d ", a[i][j]);
+        for (int j = 0; j < size; j++) {
+            printf("%d", a[i][j]);
+            if (j < size - 1) printf(" ");
+        }
         printf("\n");
     }
 
     return 0;
 }
 
-void process(int a[][20], int k, int offset) {
-    int len = 2 * k - 1;
+void process(int size, int a[][size], int offset, int value) {
 
-    // cạnh trên
-    for (int j = offset; j < offset + len; j++)
-        a[offset][j] = k;
+    // canh tren
+    for (int j = offset; j < size - offset; j++)
+        a[offset][j] = value;
 
-//    // cạnh dưới
-//    for (int j = offset; j < offset + len; j++)
-//        a[offset + len - 1][j] = k;
-//
-//    // cạnh trái
-//    for (int i = offset; i < offset + len; i++)
-//        a[i][offset] = k;
-//
-//    // cạnh phải
-//    for (int i = offset; i < offset + len; i++)
-//        a[i][offset + len - 1] = k;
+    // canh duoi
+    for (int j = offset; j < size - offset; j++)
+        a[size - 1 - offset][j] = value;
+
+    // canh trai
+    for (int i = offset; i < size - offset; i++)
+        a[i][offset] = value;
+
+    // canh phai
+    for (int i = offset; i < size - offset; i++)
+        a[i][size - 1 - offset] = value;
 }
