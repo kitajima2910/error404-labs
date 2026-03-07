17c8d7f035d022b51f25fd08c550c9a16000985a
kitajima2910 <kitajima2910@gmail.com>
2026-03-07 18:58:49 +0700

    - Add Bai6/Example.java

--- java/eclipse_workspace/Buoi3/src/vn/info/error404labs/Bai6/Example.java
+++ java/eclipse_workspace/Buoi3/src/vn/info/error404labs/Bai6/Example.java
@@ -3,16 +3,22 @@ package vn.info.error404labs.Bai6;
 public class Example {
 
 	public static void main(String[] args) {
-		
+
 		int n = 28;
-		
-		for(int i = 2; i <= n / i; i++) {
-			while(n % i == 0) {
-				System.out.println(i + " ");
+
+		for (int i = 2; i <= n / i; i++) {
+			while (n % i == 0) {
+				System.out.print(i);
 				n /= i;
+				if (n > 1) {
+					System.out.print(" x ");
+				}
 			}
 		}
-
+		
+		if(n > 1) {
+			System.out.print(n);
+		}
 	}
 
 }
