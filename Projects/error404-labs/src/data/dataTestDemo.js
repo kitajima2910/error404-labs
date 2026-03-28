710dce59427a5c2b3199759ab75444845026f2ee
kitajima2910 <kitajima2910@gmail.com>
2026-03-28 11:57:49 +0700

    - Add 2-Bat-Qua

---
+++ Projects/error404-labs/src/data/dataTestDemo.js
@@ -0,0 +1,62 @@
+export const DATA_DEMO = [
+    {
+        name: 'Bắt Quả',
+        link: '/thu-nghiem/game/2-Bat-Qua/index.html',
+        image: '/thu-nghiem/game/2-Bat-Qua/2-Bat-Qua.avif',
+    },
+    {
+        name: 'Bay Lên',
+        link: '/thu-nghiem/game/1-Bay-Len/index.html',
+        image: '/thu-nghiem/game/1-Bay-Len/1-Bay-Len.avif',
+    },
+    {
+        name: 'Chibi Runner',
+        link: '/thu-nghiem/game/ChibiRunner/build/web-desktop/index.html',
+        image: '/thu-nghiem/game/ChibiRunner/ChibiRunner.avif',
+    },
+    {
+        name: 'Thay Đổi Màu Sắc',
+        link: '/thu-nghiem/game/1-Thay-Doi-Mau-Sac/index.html',
+        image: '/thu-nghiem/game/1-Thay-Doi-Mau-Sac/1-Thay-Doi-Mau-Sac.avif',
+    },
+    {
+        name: 'Create Sanke 3D',
+        link: '/thu-nghiem/game/CreateSanke3D/index.html',
+        image: '/thu-nghiem/game/CreateSanke3D/CreateSanke3D.avif',
+    },
+    {
+        name: 'Space Ship',
+        link: '/thu-nghiem/game/space-ship/index.html',
+        image: '/thu-nghiem/game/space-ship/space-ship.avif',
+    },
+    {
+        name: 'Test Phaser Game',
+        link: '/thu-nghiem/game/TestPhaserGame/index.html',
+        image: '/thu-nghiem/game/TestPhaserGame/TestPhaserGame.avif',
+    },
+    {
+        name: 'Pet404-Labs',
+        link: '/thu-nghiem/web/pet404-labs/index.html',
+        image: '/thu-nghiem/web/pet404-labs/pet404-labs.avif',
+    },
+    {
+        name: 'Hoai-Link-Bio',
+        link: '/thu-nghiem/web/hoai-link-bio/index.html',
+        image: '/thu-nghiem/web/hoai-link-bio/hoai-link-bio.avif',
+    },
+    {
+        name: 'Vibe Waitlist',
+        link: '/thu-nghiem/web/vibe-waitlist/index.html',
+        image: '/thu-nghiem/web/vibe-waitlist/vibe-waitlist.avif',
+    },
+    {
+        name: 'Saas Directory',
+        link: '/thu-nghiem/web/saas-directory/index.html',
+        image: '/thu-nghiem/web/saas-directory/saas-directory.avif',
+    },
+    {
+        name: 'Fox Robot Slider',
+        link: '/thu-nghiem/web/FoxRobotSlider/index.html',
+        image: '/thu-nghiem/web/FoxRobotSlider/FoxRobotSlider.avif',
+    },
+]
