f5f7422eb5cf22e191628d57ddf51cf9ef0745d7
kitajima2910 <kitajima2910@gmail.com>
2026-04-05 19:37:41 +0700

    - Update login database

--- Projects/error404-labs/astro.config.mjs
+++ Projects/error404-labs/astro.config.mjs
@@ -2,20 +2,24 @@
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel'
 
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from '@tailwindcss/postcss'
 import sitemap from '@astrojs/sitemap'
 
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
     site: 'https://www.error404-labs.info.vn/',
-    integrations: [sitemap()],
+    integrations: [
+        sitemap(),
+    ],
+    vite: {
+        css: {
+            postcss: {
+                plugins: [tailwindcss()],
+            },
+        },
+    },
     markdown: {
         remarkPlugins: [remarkMath],
         rehypePlugins: [
