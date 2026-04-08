eae4b8ec26842357211196e7dad725ccbbf43e60
kitajima2910 <kitajima2910@gmail.com>
2026-04-08 12:15:48 +0700

    feat: implement navigation component with authentication status and search functionality

--- Projects/error404-labs/astro.config.mjs
+++ Projects/error404-labs/astro.config.mjs
@@ -10,6 +10,7 @@ import rehypeKatex from 'rehype-katex'
 
 export default defineConfig({
     site: 'https://www.error404-labs.info.vn/',
+    security: { checkOrigin: false },
     integrations: [
         sitemap(),
     ],
