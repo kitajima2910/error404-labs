a5b31cdee80c9fcd80314143029026263716c5b6
kitajima2910 <kitajima2910@gmail.com>
2026-04-10 20:36:37 +0700

    feat: implement navigation component and add tools data and assets

--- Projects/error404-labs/src/data/toolsData.js
+++ Projects/error404-labs/src/data/toolsData.js
@@ -2,7 +2,7 @@ export const TOOLS_DATA = [
     {
         name: 'SliceSprite',
         description: 'Tool cắt sprite sheet chuyên nghiệp cho Game Dev.',
-        link: '/tools/slicesprite-error404labs-kitajima2910/dist/index.html',
+        link: '/tools/slicesprite-error404labs-kitajima2910/distclone/index.html',
         image: 'https://images.unsplash.com/photo-1614741118887-7a4ee193a5fa?q=80&w=1887&auto=format&fit=crop',
         icon: 'fa-scissors'
     }
